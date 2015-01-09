e2dc7cb976fb97c98071f9acdfd9684b4630f046
Jorge Hernandez <jorgeh905187@gmail.com>
2015-01-09 11:25:04 -0800

    added a mouse over
    added a mouse over on my portfolio 3 website

--- public_html/script.js
+++ public_html/script.js
@@ -7,15 +7,44 @@
 
 $("document").ready(function(){
     
-    $('#oneButton').bind('click', alertButtonClick);
+   $('#oneButton').bind('click', alertButtonClick);
+   $('#oneButton').bind('click', replaceWText);
+   $('#oneButton').bind('click', replaceWHtml);
     
    $(".hero").css("background-color", "yellow");
    $('p').css("color", "blue");
     
+   $('replaceWHtml').bind('click', replaceWHtml);
+   $('replaceWText').bind('click', replaceWText);
+   
+   
 });
 
+$("h1").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
+
+$('h1').bind('click', mouseClick);
+
+
 function alertButtonClick() {
-   alert("You clicked this button"); 
+   alert("Milestone 3"); 
 }
 
+function mouseOverMe() {
+    $("h1").html("Miletone 3");
+}
 
+function mouseOutMe() {
+    $('h1').html("This is a Jquery Website");
+}
+
+function mouseClick() {
+    $('p').html('Clicked');
+}
+
+function replaceWHtml() {
+    $('#h3Tag').html('<h6>Now I\'m an h6 </h6>');
+}
+
+function replaceWText() {
+    $('#h3Tag').text('<h6>Now I\'m an h6 </h6>');
+}
